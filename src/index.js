646f4dde660a0b18e6592fc20a31cf8b1244165d
DeCrow <nonchana.s23@gmail.com>
2019-06-03 22:26:31 +0700

    implement redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './store/reducers'
 
-const store = createStore(
-  reducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose
+
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/users/index.js
+++ src/store/actions/users/index.js
@@ -1,15 +1,32 @@
 export const APP_FETCH_USER_PENDING = 'APP_FETCH_USER_PENDING'
 export const APP_FETCH_USER_SUCCESS = 'APP_FETCH_USER_SUCCESS'
 
-export function fetchUserPending() {
-  return {
-    type: APP_FETCH_USER_PENDING
+// export function fetchUserPending() {
+//   return {
+//     type: APP_FETCH_USER_PENDING
+//   }
+// }
+
+// export function fetchUserSuccess(payload) {
+//   return {
+//     type: APP_FETCH_USER_SUCCESS,
+//     payload
+//   }
+// }
+
+export const fetchUserPending = () => {
+  return dispatch => {
+    dispatch({
+      type: APP_FETCH_USER_PENDING
+    })
   }
 }
 
-export function fetchUserSuccess(payload) {
-  return {
-    type: APP_FETCH_USER_SUCCESS,
-    payload
+export const fetchUserSuccess = payload => {
+  return dispatch => {
+    dispatch({
+      type: APP_FETCH_USER_SUCCESS,
+      payload
+    })
   }
 }
