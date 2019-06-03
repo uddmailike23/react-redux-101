e791750e983e641c1774d4723384b9a52bdd3413
DeCrow <nonchana.s23@gmail.com>
2019-06-03 12:51:10 +0700

    implement provider and connect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class App extends Component {
+  onFetchUser = () => {
+    const { dispatch } = this.props
+    dispatch({
+      type: 'APP_FETCH_USER_PENDING_ES6'
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.onFetchUser}>FETCH USER</button>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App
+export default connect()(App)
--- src/index.js
+++ src/index.js
@@ -3,30 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducer'
 
 const store = createStore(reducer)
 
-console.log('store >>>', store)
-console.log('0 store.getState() >>>', store.getState())
-
-store.dispatch({
-  type: 'APP_FETCH_USER_PENDING_NORMAL'
-})
-
-console.log('1 store.getState() >>>', store.getState())
-
-store.dispatch({
-  type: 'APP_FETCH_USER_SUCCESS',
-  payload: {
-    name: 'Nonchana'
-  }
-})
-
-console.log('2 store.getState() >>>', store.getState())
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
