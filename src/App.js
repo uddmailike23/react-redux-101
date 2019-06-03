05ed21fce8f0bacb7f3f5335ad67b79b58159710
DeCrow <nonchana.s23@gmail.com>
2019-06-03 16:02:32 +0700

    implement mapStateToProps and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import './App.css'
 
 class App extends Component {
   onFetchUser = () => {
-    const { dispatch } = this.props
-    dispatch({
-      type: 'APP_FETCH_USER_PENDING_ES6'
-    })
+    const { fetchUserSuccess } = this.props
+    fetchUserSuccess()
   }
 
   render() {
+    const { myCurrentUser } = this.props
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Current user: {myCurrentUser.name}</h1>
           <button onClick={this.onFetchUser}>FETCH USER</button>
         </header>
       </div>
@@ -21,4 +21,32 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+const mapStateToProps = state => {
+  // return global state form redux store to local state
+  return {
+    myCurrentUser: state.currentUser
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUserPendingES6: () => {
+      dispatch({
+        type: 'APP_FETCH_USER_PENDING_ES6'
+      })
+    },
+    fetchUserSuccess: () => {
+      dispatch({
+        type: 'APP_FETCH_USER_SUCCESS',
+        payload: {
+          name: 'Nonchana.S'
+        }
+      })
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
